f58c7df35f3f51f86eff991839987111e3741077
seleniumthiughts <vinayakumarc11@gmail.com>
2022-09-13 12:50:14 +0530

    First commit

---
+++ src/test/java/com/gitDemoProjectv1/TC001_LoginTest.java
@@ -0,0 +1,12 @@
+package com.gitDemoProjectv1;
+
+public class TC001_LoginTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.println("User Login...");
+	}
+
+}
